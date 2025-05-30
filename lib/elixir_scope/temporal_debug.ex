b525fbed68817fe5a117ac48162faa4d394b6ed2
NSHkr <ZeroTrust@NSHkr.com>
2025-05-29 18:01:49 -1000

    Initial commit: Setup for ElixirScope.TemporalDebug library and DESIGN.md

---
+++ lib/elixir_scope/temporal_debug.ex
@@ -0,0 +1,18 @@
+defmodule ElixirScope.TemporalDebug do
+  @moduledoc """
+  Documentation for `ElixirScope.TemporalDebug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirScope.TemporalDebug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_scope/temporal_debug_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirScope.TemporalDebugTest do
+  use ExUnit.Case
+  doctest ElixirScope.TemporalDebug
+
+  test "greets the world" do
+    assert ElixirScope.TemporalDebug.hello() == :world
+  end
+end
